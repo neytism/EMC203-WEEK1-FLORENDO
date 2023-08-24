69c0ccf2bd7bc170c5acb45b8f42c37836b639db
Nate Florendo <nateodnerolf@gmail.com>
2023-08-24 20:57:13 +0800

    - Updated Unity File

--- Assets/Tower.cs
+++ Assets/Tower.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class Tower : MonoBehaviour
 {
+    public float towerRange = 5f;
+    private Transform player;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = FindObjectOfType<PlayerMovement>().gameObject.transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        IsPlayerInRange();
+    }
+
+    private void IsPlayerInRange()
+    {
+        float distance = Mathf.Sqrt(Mathf.Pow((transform.position.x - player.position.x), 2) + Mathf.Pow((transform.position.z - player.position.z), 2));
+
+        if (distance < towerRange)
+        {
+            Debug.Log("IsNear");
+        }
+        else
+        {
+            Debug.Log("IsFar");
+        }
     }
 }
